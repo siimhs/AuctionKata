b0045c971db85010af67d8dfcc355dce05bb459b
Siim Haas <siim@haas.ee>
2017-12-14 20:59:30 +0200

    Add additional properties to Auction model
    Added following properties:
    Duration - span of time from when auciton is/has been started.
    Created - instant of time when auction is/was creaed.
    Started - instant of time when auction is started.

--- src/Ares/Auction.cs
+++ src/Ares/Auction.cs
@@ -1,5 +1,10 @@
+using NodaTime;
+
 public class Auction
 {
     public int Id { get; set; }
     public Product ProductOnAuction { get; set; }
+    public Duration Duration { get; set; }
+    public Instant Created { get; set; }
+    public Instant Started { get; set; }
 }
